ad26c3b51202a53973a1c4e0e2f40a3381fa14be
Jude Hartmann <jude.hartmann@outlook.com>
2023-06-17 13:37:44 -0400

    added variables for different characters and randomness

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var lower = ["a",'b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+var capital = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+var numbers = [0,1,2,3,4,5,6,7,8,9]
+var character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", " < ", "=", " > ", " ? ", "@", "[", "/", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+var length = window.prompt("How long is your password? (between 8 and 128)")
+
+var randomLower = Math.floor(Math.random() * lower.length);
+var randomUpper = Math.floor(Math.random() * capital.length);
+var randomNumber = Math.floor(Math.random() * numbers.length);
+var randomCharacter = Math.floor(Math.random() * character.length);
+
+var lowerChoice = lower[randomLower];
+var upperChoice = capital[randomUpper];
+var numberChoice = numbers[randomNumber];
+var charChoice = character[randomCharacter];
 
 // Write password to the #password input
 function writePassword() {
@@ -12,3 +27,20 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// GIVEN I need a new, secure password
+// WHEN I click the button to generate a password
+// THEN I am presented with a series of prompts for password criteria
+// WHEN prompted for password criteria
+// THEN I select which criteria to include in the password
+// WHEN prompted for the length of the password
+// THEN I choose a length of at least 8 characters and no more than 128 characters
+// WHEN asked for character types to include in the password
+// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+// WHEN I answer each prompt
+// THEN my input should be validated and at least one character type should be selected
+// WHEN all prompts are answered
+// THEN a password is generated that matches the selected criteria
+// WHEN the password is generated
+// THEN the password is either displayed in an alert or written to the page
+// ```
